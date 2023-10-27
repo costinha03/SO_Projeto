08fd49ce1059ad6303319ba553650182650b6f5c
brunotavaresz <brunotavaresmeixedo@ua.pt>
2023-10-27 18:17:01 +0100

    #

--- spacecheck.sh
+++ spacecheck.sh
@@ -1,45 +1,38 @@
 #!/bin/bash
 
-while true; do
-    clear
-    echo "Spacecheck Menu"
-    echo "1. Visualizar espaço ocupado por todos os arquivos"
-    echo "2. Visualizar espaço ocupado por arquivos com base na expressão regular"
-    echo "3. Visualizar espaço ocupado por arquivos com base na data de modificação"
-    echo "4. Visualizar espaço ocupado por arquivos com base no tamanho mínimo"
-    echo "5. Sair"
+# Função que calcula o tamanho dos arquivos que correspondem à expressão regular
+calculate_size() {
+  local dir="$1"
+  local regex="$2"
 
-    read -p "Escolha uma opção: " option
+  find "$dir" -type f -name "$regex" -exec du -b {} + | awk '{s+=$1} END {print s}'
+}
 
-    case $option in
-        1)
-            # Opção para visualizar espaço ocupado por todos os arquivos
-            ./spacecheck.sh
-            ;;
-        2)
-            # Opção para visualizar espaço ocupado com base na expressão regular
-            read -p "Digite a expressão regular: " regex
-            ./spacecheck.sh -n "$regex"
-            ;;
-        3)
-            # Opção para visualizar espaço ocupado com base na data de modificação
-            read -p "Digite a data máxima de modificação (YYYY-MM-DD): " date
-            ./spacecheck.sh -d "$date"
-            ;;
-        4)
-            # Opção para visualizar espaço ocupado com base no tamanho mínimo
-            read -p "Digite o tamanho mínimo (em bytes): " size
-            ./spacecheck.sh -s "$size"
-            ;;
-        5)
-            # Opção para sair do menu
-            echo "Saindo do programa."
-            exit
-            ;;
-        *)
-            echo "Opção inválida. Tente novamente."
-            ;;
-    esac
+# Verificar se o número de argumentos é válido
+if [ "$#" -ne 3 ]; then
+  echo "Uso: $0 -n <expressão-regular> <diretório>"
+  exit 1
+fi
 
-    read -p "Pressione Enter para continuar..."
-done
+# Verificar se a opção é -n
+if [ "$1" != "-n" ]; then
+  echo "Opção inválida. Use -n para especificar a expressão regular."
+  exit 1
+fi
+
+# Atribuir a expressão regular e o diretório a variáveis
+regex="$2"
+dir="$3"
+
+# Verificar se o diretório existe
+if [ ! -d "$dir" ]; then
+  echo "O diretório especificado não existe."
+  exit 1
+fi
+
+# Chamar a função para calcular o tamanho dos arquivos
+size=$(calculate_size "$dir" "$regex")
+
+# Exibir os resultados
+echo "SIZE NAME $(date +"%Y%m%d") -n $regex $dir"
+echo "$size $dir"
