5ab85d75ce8e96e1821604e7fd8f4342ce5e84bf
brunotavaresz <brunotavaresmeixedo@ua.pt>
2023-11-13 18:27:45 +0000

    spacerate a funciminare

--- test_a1/spacerate.sh
+++ test_a1/spacerate.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-
 sorting_order="default"
+header_printed=false
 
 # Parse command-line options
-while getopts "ra" option; do # Obter as opções da linha de comando -r e -a
+while getopts "ra" option; do
   case $option in
     r)
-      sorting_order="reverse" # ordem inversa
+      sorting_order="reverse"
       ;;
     a)
-      sorting_order="alphabetical" # ordem alfabética
+      sorting_order="alphabetical"
       ;;
     *)
-      echo "Usage: $0 [-r] [-a] <file1> <file2>" # Se a opção for diferente de -r e -a, imprima a mensagem de erro
+      echo "Usage: $0 [-r] [-a] <file1> <file2>"
       exit 1
       ;;
   esac
@@ -21,64 +21,58 @@ done
 
 shift $((OPTIND - 1))
 
-
-if [ $# -ne 2 ]; then # Se o número de argumentos for diferente de 2, imprima a mensagem de erro (ne = not equal)
-  echo "Usage: $0 [-r] [-a] <file1> <file2>" # Se a opção for diferente de -r e -a, imprima a mensagem de erro
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 [-r] [-a] <file1> <file2>"
   exit 1
 fi
 
-# Lê dois arquivos vindo dos argumentos da linha de comando
 file1="$1"
 file2="$2"
 
-# Função para comparar os arquivos
-compare_files() {
+# Ensure header is always printed first
+echo "SIZE NAME"
 
+compare_files() {
   declare -A dir_sizes1 
   declare -A dir_sizes2 
 
-
-  while read -r size dir; do # Lê o primeiro arquivo e armazena os tamanhos dos diretórios
-    dir_sizes1["$dir"]=$size # Armazena o tamanho do diretório no array associativo dir_sizes1
+  while read -r size dir; do
+    dir_sizes1["$dir"]=$size
   done < "$file1"  
 
-  while read -r size dir; do # Lê o segundo arquivo e armazena os tamanhos dos diretórios
-    dir_sizes2["$dir"]=$size # Armazena o tamanho do diretório no array associativo dir_sizes2
+  while read -r size dir; do
+    # Check if the line contains two fields before assigning values
+    if [ -n "$size" ] && [ -n "$dir" ]; then
+      dir_sizes2["$dir"]=$size
+    fi
   done < "$file2"
 
-  echo "SIZE NAME" #imprime o cabeçãlho
-  
-  for dir in "${!dir_sizes1[@]}"; do # Percorre o array associativo dir_sizes1
-    size1="${dir_sizes1[$dir]}" # Armazena o tamanho do diretório no array associativo dir_sizes1
-    size2="${dir_sizes2[$dir]}" # Armazena o tamanho do diretório no array associativo dir_sizes2
+  for dir in "${!dir_sizes1[@]}"; do
+    size1="${dir_sizes1[$dir]}"
+    size2="${dir_sizes2[$dir]}"
 
     if [ -z "$size2" ]; then
-      # Diretorio presente apenas no primeiro arquivo
-      echo "$size_diff $dir REMOVED" # Imprime o tamanho do diretório e o nome do diretório com a mensagem REMOVED
+      echo "-$size1 $dir REMOVED"
     else
-      # Diretorio presente em ambos os arquivos
-      size_diff=$((size2 - size1)) # Calcula a diferença entre os tamanhos dos diretórios (size2 - size1)
-      echo "$size_diff $dir" # Imprime a diferença entre os tamanhos dos diretórios e o nome do diretório
+      size_diff=$((size2 - size1))
+      echo "$size_diff $dir"
     fi
   done
 
-  # Diretorios presentes apenas no segundo arquivo
-  for dir in "${!dir_sizes2[@]}"; do # Percorre o array associativo dir_sizes2
-    size1="${dir_sizes1[$dir]}" # Armazena o tamanho do diretório no array associativo dir_sizes1
-    size2="${dir_sizes2[$dir]}" # Armazena o tamanho do diretório no array associativo dir_sizes2
+  for dir in "${!dir_sizes2[@]}"; do
+    size1="${dir_sizes1[$dir]}"
+    size2="${dir_sizes2[$dir]}"
 
-    if [ -z "${dir_sizes1[$dir]}" ]; then # Se o tamanho do diretório for vazio (não presente no primeiro arquivo)
-      echo "${dir_sizes2[$dir]} $dir NEW" # Imprime o tamanho do diretório e o nome do diretório com a mensagem NEW
+    if [ -z "${dir_sizes1[$dir]}" ]; then
+      echo "${dir_sizes2[$dir]} $dir NEW"
     fi
   done
 }
 
-# Chama a função compare_files e ordena os diretórios de acordo com a opção escolhida (default, ordem inversa ou ordem alfabética)
-if [ "$sorting_order" == "reverse" ]; then # Se a ordem for inversa
-  compare_files "$file1" "$file2" | sort -rn # Ordena os diretórios em ordem inversa e imprime o tamanho e o nome do diretório
-elif [ "$sorting_order" == "alphabetical" ]; then # Se a ordem for alfabética
-  compare_files "$file1" "$file2" | sort -k2 # Ordena os diretórios em ordem alfabética e imprime o tamanho e o nome do diretório
+if [ "$sorting_order" == "reverse" ]; then
+  compare_files "$file1" "$file2" | tac
+elif [ "$sorting_order" == "alphabetical" ]; then
+  compare_files "$file1" "$file2" | sort -k2
 else
-# Se a ordem for padrão
-  compare_files "$file1" "$file2" # Imprime o tamanho e o nome do diretório 
+  compare_files "$file1" "$file2"
 fi
